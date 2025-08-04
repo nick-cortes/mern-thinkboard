b1b78dd631ec240a2aca84e29cf0373299450b08
Nicholes Cortes <187998850+ncortes9@users.noreply.github.com>
2025-08-04 19:34:47 -0400

    Add a get individual note handler

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -2,13 +2,24 @@ import Note from "../models/Note.js";
 
 export async function getAllNotes(req, res) {
     try {
-        const notes = await Note.find();
+        const notes = await Note.find().sort({createdAt:-1});
         res.status(200).json(notes);
     } catch(error) {
         console.error("Error in getAllNotes controller");
         res.status(500).json({message:"Internal server error"});
     }
 };
+
+export async function getNoteById(req, res) {
+    try {
+        const note = await Note.findById(req.params.id);
+        if (!note) return res.status(404).json({message:"Note not found"});
+        res.json(note);
+    } catch(error) {
+        console.error("Error in getAllNotes controller", error);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
 
 export async function createNote(req, res) {
     try {
--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createNote, deleteNote, getAllNotes, updateNote } from "../controllers/notesController.js"; 
+import { createNote, getNoteById, deleteNote, getAllNotes, updateNote } from "../controllers/notesController.js"; 
 
 const router = express.Router();
 
 router.get("/", getAllNotes);
+
+router.get("/:id", getNoteById);
 
 router.post("/", createNote);
 
