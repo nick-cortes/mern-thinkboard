49a4acb9a1fa3bf8936ee5a1a80e2321debbb275
Nicholes Cortes <187998850+ncortes9@users.noreply.github.com>
2025-08-11 20:47:17 -0400

    home page

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors"; 
+import dotenv from "dotenv";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
@@ -9,9 +11,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+// middleware
+app.use(cors({
+    origin: "http://localhost:5173",
+}));
 
 app.use(express.json());
 app.use(rateLimiter);
+
 
 app.use("/api/notes", notesRoutes)
 
